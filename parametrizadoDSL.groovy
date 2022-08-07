5fbb4441ef0fc8792d12f0ac6b8bc95a55756f6f
JAVIER Angel <angel.javier@edenred.com>
2022-08-06 19:38:19 -0500

    Second commit

---
+++ parametrizadoDSL.groovy
@@ -0,0 +1,40 @@
+job('ejemplo2-job-DSL') {
+	description('Job DSL de ejemplo para el curso de Jenkins')
+  	scm {
+      		git('https://github.com/AngelJava78/jenkins.job.parametrizado', 'main') { node ->
+        		node / gitConfigName('AngelJava78')
+        		node / gitConfigEmail('angel_java@hotmail.com')
+      		}
+    	} 
+  	parameters {
+   		stringParam('nombre', defaultValue = 'Julian', description = 'Parametro de cadena para el Job Booleano')
+      		choiceParam('planeta', ['Mercurio', 'Venus', 'Tierrra', 'Marte', 'Jupiter', 'Saturno', 'Urano', 'Neptuno'])
+      		booleanParam('agente', false)
+    	}
+  	triggers {
+    		cron('H/7 * * * *')
+    	}
+  	steps {
+    		shell("bash jobscript.sh")
+    	}
+  	publishers {
+      		mailer('macloujulian@gmail.com', true, true)
+      		slackNotifier {
+		  notifyAborted(true)
+		  notifyEveryFailure(true)
+		  notifyNotBuilt(false)
+		  notifyUnstable(false)
+		  notifyBackToNormal(true)
+		  notifySuccess(false)
+		  notifyRepeatedFailure(false)
+		  startNotification(false)
+		  includeTestSummary(false)
+		  includeCustomMessage(false)
+		  customMessage(null)
+		  sendAs(null)
+		  commitInfoChoice('NONE')
+		  teamDomain(null)
+		  authToken(null)
+        	}
+    	}
+}
