b4c6cbfb4fd7f9bbbdf518822c317a04dcbfb808
JAVIER Angel <angel.javier@edenred.com>
2022-08-10 18:24:25 -0500

    9o. commit

--- jobscript.sh
+++ jobscript.sh
@@ -6,8 +6,7 @@ then
 else
 	echo "Disfruta el planeta humano/a $nombre, trata de no morir."
 fi
-echo "Apellido paterno: $apellido"
-echo "Edad: $edad"
+echo "Apellido paterno: $apellido. Edad: $edad."
 echo "..."
 sleep 10
 echo "¡Suerte! La necesitarás."
